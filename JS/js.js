b94ba1cc5ad132bc1256bf4f3a3e246d7e1a5a5a
Jameslabrie <jameslabrie@bk.ru>
2023-01-13 15:55:25 +0400

    Fixed: bugs in cycle for. Create Comeback

--- JS/js.js
+++ JS/js.js
@@ -1,18 +1,34 @@
-let money = prompt("Ваш бюджет на месяц?"),
-  time = prompt("Введите дату в формате YYYY-MM-DD"),
-  expensesName = prompt("Введите обязательную статью расходов на месяц"),
-  expenses = prompt("Во сколько обойдется?"),
-  expensesNameTwo = prompt("Введите обязательную статью расходов на месяц"),
-  expensesTwo = prompt("Во сколько обойдется?");
+let money = +prompt("Ваш бюджет на месяц?"),
+  time = prompt("Введите дату в формате YYYY-MM-DD");
 
-let appData = new Object();
-appData.budget = money;
-appData.timeData = time;
-appData.expenses = { expensesName: expenses, expensesNameTwo: expensesTwo };
-appData.optionalExpenses;
-appData.income = [];
-appData.savings = false;
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: false,
+};
 
-let budgetInOneDay = (money + appData.income - expenses - expensesTwo) / 30;
+for (let i = 0; i < 2; i++) {
+  let question1 = prompt("Введите обязательную статью расходов на месяц"),
+    question2 = prompt("Во сколько обойдется?");
+
+  if (
+    typeof question1 == "string" &&
+    typeof question1 != null &&
+    typeof question2 != null &&
+    question1 != "" &&
+    question2 != "" &&
+    question1.length < 20 &&
+    question2.length < 10
+  ) {
+    appData.expenses[question1] = question2;
+  } else {
+    i = 0;
+  }
+}
+
+let budgetInOneDay = appData.budget / 30;
 
 alert("Ваш бюджет на один день: " + budgetInOneDay);
