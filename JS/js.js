a7bbe52e119342a3d026dd67cff46b6f8b2e0586
Jameslabrie <jameslabrie@bk.ru>
2023-01-13 16:49:18 +0400

    Create function detectDayBudget

--- JS/js.js
+++ JS/js.js
@@ -1,34 +1,48 @@
-let money = +prompt("Ваш бюджет на месяц?"),
-  time = prompt("Введите дату в формате YYYY-MM-DD");
+let money, time;
 
-let appData = {
-  budget: money,
-  timeData: time,
-  expenses: {},
-  optionalExpenses: {},
-  income: [],
-  savings: false,
-};
-
-for (let i = 0; i < 2; i++) {
-  let question1 = prompt("Введите обязательную статью расходов на месяц"),
-    question2 = prompt("Во сколько обойдется?");
-
-  if (
-    typeof question1 == "string" &&
-    typeof question1 != null &&
-    typeof question2 != null &&
-    question1 != "" &&
-    question2 != "" &&
-    question1.length < 20 &&
-    question2.length < 10
-  ) {
-    appData.expenses[question1] = question2;
-  } else {
-    i = 0;
+function detectDayBudget() {
+  function start() {
+    money = +prompt("Ваш бюджет на месяц?");
+    time = prompt("Введите дату в формате YYYY-MM-DD");
+    while (isNaN(money) || money == "" || money == null) {
+      money = +prompt("Ваш бюджет на месяц?");
+    }
   }
+  start();
+
+  let appData = {
+    budget: money,
+    timeData: time,
+    expenses: {},
+    optionalExpenses: {},
+    income: [],
+    savings: false,
+  };
+
+  function expenses() {
+    for (let i = 0; i < 2; i++) {
+      let question1 = prompt("Введите обязательную статью расходов на месяц"),
+        question2 = prompt("Во сколько обойдется?");
+
+      if (
+        typeof question1 == "string" &&
+        typeof question1 != null &&
+        typeof question2 != null &&
+        question1 != "" &&
+        question2 != "" &&
+        question1.length < 20 &&
+        question2.length < 10
+      ) {
+        appData.expenses[question1] = question2;
+      } else {
+        i = i - 1;
+      }
+    }
+  }
+  expenses();
+
+  let budgetInOneDay = appData.budget / 30;
+
+  alert("Ваш бюджет на один день: " + budgetInOneDay);
 }
-
-let budgetInOneDay = appData.budget / 30;
-
-alert("Ваш бюджет на один день: " + budgetInOneDay);
+detectDayBudget();
