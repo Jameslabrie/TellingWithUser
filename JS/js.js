234623839cd41263cbb8d6e9e5447cbf8986e130
Jameslabrie <jameslabrie@bk.ru>
2023-01-13 19:59:47 +0400

    Create: update appData

--- JS/js.js
+++ JS/js.js
@@ -1,25 +1,23 @@
-let money, time;
+let money, time, budgetInOneDay, optExpenses;
 
-function detectDayBudget() {
-  function start() {
+function start() {
+  money = +prompt("Ваш бюджет на месяц?");
+  time = prompt("Введите дату в формате YYYY-MM-DD");
+
+  while (isNaN(money) || money == "" || money == null) {
     money = +prompt("Ваш бюджет на месяц?");
-    time = prompt("Введите дату в формате YYYY-MM-DD");
-    while (isNaN(money) || money == "" || money == null) {
-      money = +prompt("Ваш бюджет на месяц?");
-    }
   }
-  start();
+}
+start();
 
-  let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {},
-    optionalExpenses: {},
-    income: [],
-    savings: false,
-  };
-
-  function expenses() {
+let appData = {
+  budget: money,
+  timeData: time,
+  expenses: {},
+  optionalExpenses: {},
+  income: [],
+  savings: true,
+  chooseExpenses: function () {
     for (let i = 0; i < 2; i++) {
       let question1 = prompt("Введите обязательную статью расходов на месяц"),
         question2 = prompt("Во сколько обойдется?");
@@ -38,11 +36,40 @@ function detectDayBudget() {
         i = i - 1;
       }
     }
-  }
-  expenses();
+  },
+  detectDayBudget: function () {
+    appData.budgetInOneDay = (appData.budget / 30).toFixed(1);
 
-  let budgetInOneDay = appData.budget / 30;
+    alert("Ваш бюджет на один день: " + appData.budgetInOneDay);
+  },
+  detectLevel: function () {
+    if (appData.budgetInOneDay < 50) {
+      alert("Нужно ужаться!");
+    } else if (appData.budgetInOneDay > 50 && appData.budgetInOneDay < 130) {
+      alert("Вы можете жить хорошо!");
+    } else {
+      alert("Ваша жизнь полна красок!");
+    }
+  },
+  chooseOptExpenses: function () {
+    for (let i = 0; i < 3; i++) {
+      let opt = prompt("Статья необязательных расходов?", "");
+      appData.optionalExpenses[i] = opt;
+    }
+  },
+  checkSavings: function () {
+    if (appData.savings == true) {
+      let save = +prompt("Какова сумма ваших накоплений?"),
+        procent = +prompt("Под какой процент?");
 
-  alert("Ваш бюджет на один день: " + budgetInOneDay);
-}
-detectDayBudget();
+      appData.monthIncome = (save / 100 / 12) * procent;
+      alert("Сумма заработка за месяц = " + appData.monthIncome);
+    }
+  },
+};
+
+appData.chooseExpenses();
+appData.detectDayBudget();
+appData.detectLevel();
+appData.checkSavings();
+appData.chooseOptExpenses();
